07462e3e0b094408d53aec7115a71ad4d268f08d
pabu10001 <pablo.hchez@gmail.com>
2022-02-28 03:26:29 -0800

    Add project files.

---
+++ SubirNotaWPF/MiComponente/Encriptacion.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SubirNotaWPF.MiComponente
+{
+    /// <summary>
+    /// Interaction logic for Encriptacion.xaml
+    /// </summary>
+    public partial class Encriptacion : UserControl, INotifyPropertyChanged
+    {
+        public string tipo_encriptacion { get; set; }
+        private String input = "";
+        private String output = "";
+        public Encriptacion()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void encriptar_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Output = LogicaEncriptacion.encriptar(Input, tipo_encriptacion, txt_key.Text);
+        }
+
+        private void desencriptar_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Output = LogicaEncriptacion.desencriptar(Input, tipo_encriptacion, txt_key.Text);
+        }
+
+        public String Input
+        {
+            get
+            {
+                return this.input;
+            }
+
+            set
+            {
+                if (value != this.input)
+                {
+                    this.input = value;
+                    OnPropertyChanged("Input");
+                }
+            }
+        }
+
+        public String Output
+        {
+            get
+            {
+                return this.output;
+            }
+
+            set
+            {
+                if (value != this.output)
+                {
+                    this.output = value;
+                    OnPropertyChanged("Output");
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SubirNotaWPF/MiComponente/LogicaEncriptacion.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace SubirNotaWPF.MiComponente
+{
+    internal class LogicaEncriptacion
+    {
+        static string abc = "abcdefghijklmñnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890_-+,#$%&/()=¿?¡!|,.;:{}[]";
+        public static string encriptar(string texto, string tipo_encriptacion, string clave)
+        {
+            switch (tipo_encriptacion)
+            {
+                case "cesar":
+                    try
+                    {
+                        int clave_int = Convert.ToInt32(clave);
+                        return cesar_enc(texto, clave_int);
+                    }
+                    catch (Exception ex) { return "clave no valida"; }
+                    break;
+                default:
+                    return "tipo de encriptacion no valido";
+            }
+
+        }
+
+        public static string desencriptar(string texto, string tipo_encriptacion, string clave)
+        {
+            switch (tipo_encriptacion)
+            {
+                case "cesar":
+                    try
+                    {
+                        int clave_int = Convert.ToInt32(clave);
+                        return cesar_dec(texto, clave_int);
+                    }
+                    catch (Exception ex) { return "clave no valida"; }
+                    break;
+                default:
+                    return "tipo de desencriptacion no valido";
+            }
+        }
+
+        static string cesar_enc(string mensaje, int desplazamiento)
+        {
+            String cifrado = "";
+            desplazamiento = desplazamiento % abc.Length;
+
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                int posCaracter = getPosABC(mensaje[i]);
+                if (posCaracter != -1)
+                {
+                    int pos = (posCaracter + desplazamiento) % abc.Length;
+                    cifrado += abc[pos];
+                }
+                else
+                {
+                    cifrado += mensaje[i];
+                }
+            }
+
+
+            return cifrado;
+        }
+
+        static string cesar_dec(string mensaje, int desplazamiento)
+        {
+            String cifrado = "";
+            desplazamiento = desplazamiento % abc.Length;
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                int posCaracter = getPosABC(mensaje[i]);
+                if (posCaracter != -1) //el caracter existe en la variable abc
+                {
+                    int pos = (posCaracter + (abc.Length - desplazamiento)) % abc.Length;
+                    cifrado += abc[pos];
+                }
+                else
+                {
+                    cifrado += mensaje[i];
+                }
+            }
+            return cifrado;
+        }
+
+        static int getPosABC(char caracter)
+        {
+            for (int i = 0; i < abc.Length; i++)
+            {
+                if (caracter == abc[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
